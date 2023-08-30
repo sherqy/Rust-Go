9165a1a113cd8c54f472968f6ce3e23ad248abf1
YoungHwan <71543621+sherqy@users.noreply.github.com>
2023-08-30 13:44:55 +0000

    first build

---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+/workspaces/Rust-Go/Rust/target/debug/Rust: /workspaces/Rust-Go/Rust/src/main.rs
---
+++ Rust/target/debug/deps/Rust-142f8773e3fd3ae9.d
@@ -0,0 +1,5 @@
+/workspaces/Rust-Go/Rust/target/debug/deps/Rust-142f8773e3fd3ae9: src/main.rs
+
+/workspaces/Rust-Go/Rust/target/debug/deps/Rust-142f8773e3fd3ae9.d: src/main.rs
+
+src/main.rs:
